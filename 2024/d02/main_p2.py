852decdfeb45fdd0c7323adb4758754e50fcf166
Jan Niklas Diercks <janniklas.diercks@gmail.com>
2024-12-02 23:16:47 +0100

    fix: remove debug prints

--- 2024/d02/main_p2.py
+++ 2024/d02/main_p2.py
@@ -11,13 +11,11 @@ def isSafeReport(pairwiseDifferences):
     return 1 <= maxDifference <= 3 and (allIncreasing or allDecreasing)
 
 def getFirstNonNegatveIndex(list):
-    print("first positive:", list)
     for idx, x in enumerate(list):
       if x >= 0:
         return idx
 
 def getFirstNonPositiveIndex(list):
-    print("first negative:", list)
     for idx, x in enumerate(list):
       if x <= 0:
         return idx
@@ -30,13 +28,11 @@ def isSingleBadLevelReport(pairwiseDifferences):
 
     if almostAllIncreasing or increasingCount == 1:
       negativeIndex = getFirstNonPositiveIndex(pairwiseDifferences)
-      print("n index:" , negativeIndex)
       del pairwiseDifferences[negativeIndex]
       if isSafeReport(pairwiseDifferences):
         return True
     if almostAllDecreasing or decreasingCount == 1:
       positiveIndex = getFirstNonNegatveIndex(pairwiseDifferences)
-      print("p index:" , positiveIndex)
       del pairwiseDifferences[positiveIndex]
       if isSafeReport(pairwiseDifferences):
         return True
@@ -51,21 +47,14 @@ with open(filePath, 'r') as f:
   isValid = True 
   safeReportCount = 0
   for l in data:
-    print("------------------------")
     values = re.split(r"\s+", l)
-    print("values: ", values)
     pairwiseDifferences = [int(x[1]) - int(x[0]) for x in pairwise(values)]
-    print("pairs: ", pairwiseDifferences)
     if isSafeReport(pairwiseDifferences):
-      print("Is safe: ", l)
       safeReportCount = safeReportCount + 1
     elif isSingleBadLevelReport(pairwiseDifferences):
-      print("Potentionally unsafe is safe by removeing one level:", l)
       safeReportCount = safeReportCount + 1
     else:
        print("UNSAFE: ", values)
     
-
-
   print("Safe reports:")
   print(safeReportCount)
