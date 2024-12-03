8a866bb0afd1d942f9e924ca7b6ecbe68dad0cf8
Jan Niklas Diercks <janniklas.diercks@gmail.com>
2024-12-03 22:19:06 +0100

    feat: add solution for day 3

---
+++ 2024/d03/main_p2.py
@@ -0,0 +1,13 @@
+import re
+  
+test = False
+filePath = './test_input.txt' if test else './input.txt'
+with open(filePath, 'r') as f:
+  data = f.read()
+  data = re.sub(r"don't\(\)(.|\n)*?(?=do\(\)|$)", "", data)
+  regex = r"mul\((\d{1,3}),(\d{1,3})\)"
+  matches = re.finditer(regex, data, re.MULTILINE)
+
+  matchedGroups = [x.groups() for x in matches]
+  result = sum([int(x[0])*int(x[1]) for x in matchedGroups])
+  print(result)
