e451a4f6086cfcd0fb3c71695bd934fd6a12fcc9
Jan Niklas Diercks <janniklas.diercks@gmail.com>
2024-12-23 09:02:27 +0100

    perf(d22): switch from concurrent dictionary to dictionary

--- 2024/d22/Day22.cs
+++ 2024/d22/Day22.cs
@@ -1,15 +1,16 @@
 ﻿namespace AdventOfCode.Y2024;
-using Cache = System.Collections.Concurrent.ConcurrentDictionary<int, int>;
+using Cache = Dictionary<int, int>;
 
 public class Day22 : ISolver
 {
     public object PartOne(string input, bool test)
     {
         var secretNumbers = input.Split("\n");
-        var secretNumberCount = 10;
+        var secretNumberCount = 2000;
         ulong result = 0;
-        
-        foreach (var secretNumber in secretNumbers) {
+
+        foreach (var secretNumber in secretNumbers)
+        {
             var nextSecret = CalculateSecretNumber(ulong.Parse(secretNumber), secretNumberCount);
             result += nextSecret;
         }
@@ -22,49 +23,65 @@ public class Day22 : ISolver
         var secretNumberCount = 2000;
         ulong result = 0;
         var cache = new Cache();
-        
-        foreach (var secretNumber in secretNumbers) {
+
+        foreach (var secretNumber in secretNumbers)
+        {
             var nextSecret = CalculateBestSequence(ulong.Parse(secretNumber), secretNumberCount, cache);
             result += nextSecret;
         }
         return cache.Values.Max();
     }
-    public ulong CalculateBestSequence(ulong secret, int count, Cache cache) {
+    public ulong CalculateBestSequence(ulong secret, int count, Cache cache)
+    {
         var secretNumber = secret;
         int buffer = 0;
         var uniques = new HashSet<int>();
 
         var price = (ushort)(secretNumber % 10);
-        for (int i = 0; i < count; i++) {
-           secretNumber = CalculateSecretNumber(secretNumber); 
+        for (int i = 0; i < count; i++)
+        {
+            secretNumber = CalculateSecretNumber(secretNumber);
             ushort nextPrice = (ushort)(secretNumber % 10);
             buffer <<= 8;
             byte diff = (byte)(sbyte)(nextPrice - price);
             // Utils.Print((sbyte)diff);
             buffer |= diff;
-            price = nextPrice; 
-            if (i >= 3 && uniques.Add(buffer)) {
-                cache.AddOrUpdate(buffer, price, (key, oldValue) => oldValue + price);
+            price = nextPrice;
+            if (i >= 3 && uniques.Add(buffer))
+            {
+                if (cache.TryGetValue(buffer, out var p))
+                {
+                    cache[buffer] = p + price;
+                }
+                else
+                {
+                    cache.Add(buffer, price);
+                }
+                // Previously used for concurrent dictionnry
+                // cache.AddOrUpdate(buffer, price, (key, oldValue) => oldValue + price);
             }
         }
         return secretNumber;
     }
-    
-    public ulong CalculateSecretNumber(ulong secret, int count) {
+
+    public ulong CalculateSecretNumber(ulong secret, int count)
+    {
         var secretNumber = secret;
-        for (int i = 0; i < count; i++) {
-           secretNumber = CalculateSecretNumber(secretNumber); 
+        for (int i = 0; i < count; i++)
+        {
+            secretNumber = CalculateSecretNumber(secretNumber);
         }
         return secretNumber;
     }
-    
-    public ulong CalculateSecretNumber(ulong number) {
-		number = Prune(Mix(number, number * 64));
-		number = Prune(Mix(number, number / 32));
-		number = Prune(Mix(number, number * 2048));
-		return number;
+
+    public ulong CalculateSecretNumber(ulong number)
+    {
+        number = Prune(Mix(number, number << 6));
+        number = Prune(Mix(number, number >> 5));
+        number = Prune(Mix(number, number << 11));
+        return number;
     }
-    
+
     public ulong MixPrune(ulong secret, ulong number) => Prune(Mix(secret, number));
     public ulong Mix(ulong secret, ulong number) => secret ^ number;
     public ulong Prune(ulong secret) => secret % 16777216;
